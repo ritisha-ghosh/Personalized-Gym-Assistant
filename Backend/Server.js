0764f014c3e65e0c4c95060652ce8642e1a0d09b
Subham Roy <r.subham4334@gmail.com>
2026-01-29 14:10:45 +0530

    Update my branch

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,6 +16,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/diet", dietRoutes);
 app.use("/api/diet-plan", dietPlanRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
