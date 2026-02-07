2a61deabc728f5767d18125afd1fa270d21f4d70
Shrexya <shreyasarkar200317@gmail.com>
2026-02-07 13:02:58 +0530

    Add JWT refresh token authentication flow

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { signup, login } = require("../controllers/authController");
+const { signup, login, refreshAccessToken } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);   // 
+router.post("/refresh-token", refreshAccessToken);
+
 
 module.exports = router;
